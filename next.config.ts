55bf819bf2946ae731a83a10d682f2bff030e4c0
0Apexx0 <adityaapexx@gmail.com>
2025-05-03 05:11:54 +0530

    changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
 };
 
-export default nextConfig;
+export default config;
